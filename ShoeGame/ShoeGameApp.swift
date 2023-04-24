3bea4e73dc07abb99bc6d0a25eb774549c579977
E. Hannah <echannah631@gmail.com>
2023-04-24 17:22:26 -0500

    Initial Commit

---
+++ ShoeGame/ShoeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeGameApp.swift
+//  ShoeGame
+//
+//  Created by Elliot Hannah III on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
