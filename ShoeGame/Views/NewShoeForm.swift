5ca938a8fd151a9de4af157389caeafd82f7baa3
E. Hannah <echannah631@gmail.com>
2023-04-25 22:25:51 -0500

    added loading state to new shoe form

--- ShoeGame/Views/NewShoeForm.swift
+++ ShoeGame/Views/NewShoeForm.swift
@@ -12,14 +12,17 @@ struct NewShoeForm: View {
     
     let addAction: AddAction
     @State private var shoe = Shoe()
+    @State private var state = FormState.idle
     @Environment(\.dismiss) var dismiss
         
     private func saveShoe() {
         Task {
+            state = .working
             do {
                 try await addAction(shoe)
             } catch {
                 print("[NewShoeForm] Cannot add shoe to collection: \(error)")
+                state = .error
             }
         }
     }
@@ -80,6 +83,8 @@ struct NewShoeForm: View {
                 }
             }
         }
+        .disabled(state == .working)
+        .overlay(workingOverlay)
         .navigationTitle("Add To Collection")
         .navigationBarBackButtonHidden(true)
         .toolbar {
@@ -99,6 +104,36 @@ struct NewShoeForm: View {
                 .disabled(shoe.brand == .none || shoe.model.isEmpty || shoe.colorWay.isEmpty || shoe.dominantMaterial == .none)
             }
         }
+        .alert("Cannot create shoe", isPresented: $state.isError) {
+            Text("Sorry, something wen't wrong")
+        }
+    }
+    
+    @ViewBuilder private var workingOverlay: some View {
+        if state == .working {
+            ZStack {
+                Color(white: 0, opacity: 0.35)
+                ProgressView().tint(.white)
+            }
+        }
+    }
+}
+
+extension NewShoeForm {
+    enum FormState {
+        case idle, working, error
+
+        //This will be used for the isPresented variable for the alert modifier
+        //When the alert is dismissed, the value of it will be flipped and the state will be set to idle
+        var isError: Bool {
+            get {
+                self == .error
+            }
+            set {
+                guard !newValue else {return}
+                self = .idle
+            }
+        }
     }
 }
 
@@ -107,6 +142,5 @@ struct NewShoeForm_Previews: PreviewProvider {
         NavigationView {
             NewShoeForm(addAction: { _ in })
         }
-        
     }
 }
